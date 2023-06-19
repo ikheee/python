796efcb36340c0a23740172233a3ac8bd0e4e336
shinikhee <ikheee@naver.com>
2023-06-20 07:38:32 +0900

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+print(10%3)
+print(10//3)
+
+print(3 == 3)
+print(3 != 3)
+print(3 > 3)
+print(3 > 2 > 1)
+
+print(not(True))
+
+print(True and True)
+print(True & False)
+
+print(True or True)
+print(True | False)
+
+print("한글")
